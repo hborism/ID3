9eb1065cdb19a764d2f4eaeabbaafbef86ffcee2
hborism <henrik.borismoller@gmail.com>
2016-02-28 17:14:49 +0100

    added some recursion and modified gain but they do not work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import math
+import copy
 
 
 
@@ -177,13 +178,16 @@ class Tree:
             return 0
         return -(q*math.log(q,2)+(1-q)*math.log(1-q,2))
 
+    def getRoot(self):
+        return self.__root
+
     def getAttributes(self):
         return self.__attributes
 
     def goalEntropy(self):
         return self.__goal_entropy
 
-    def gain(self, attribute, examples):
+    def gain(self, attribute, sorted_examples):
         if attribute not in self.__attributes:
             return -1
 
@@ -192,21 +196,29 @@ class Tree:
         while self.__attributes[attribute_pos]!=attribute:
             attribute_pos+=1
 
+        #calculate the number of examples
+        no_examples=len(sorted_examples[0])+len(sorted_examples[1])
+        if no_examples==0:
+            return 0
+
         #creating a tree for this attribute (look at fig 18.4 in book)
         no_options=len(self.__options[attribute_pos])
         result=[]
         for c in range(0,no_options):
             result.append([0,0])
 
-        for i in range(0, self.__no_examples):
-            result[self.__data[i][attribute_pos]][self.__data[i][self.__goal_pos]]+=1
+        for outcome in sorted_examples:
+            for i in range(0, len(outcome)):
+                result[outcome[i][attribute_pos]][outcome[i][self.__goal_pos]]+=1
 
 
         #calculate the remainder
         remainder=0
         c=0
         for c in range(0, no_options):
-            remainder+=(sum(result[c])/self.__no_examples)*(self.entropy(result[c][0]/sum(result[c])))
+            if sum(result[c])!=0:
+                remainder+=(sum(result[c])/no_examples)*(self.entropy(result[c][0]/sum(result[c])))
+
 
         #return goal entropy minus the remainder (see page 704 in book)
         return self.__goal_entropy-remainder
@@ -238,26 +250,80 @@ class Tree:
         return maxgain_attribute
 
     def buildTree(self):
-        qwe=self.__attributes
-        qwe.remove("goal")
-        print(self.importance(qwe,self.__data))
+        attributes=copy.copy(self.__attributes)
+        attributes.remove(self.__goal)
+
+        attribute=self.importance(attributes,self.sortExamples(self.__data))
+        options=self.__options[self.__attributes.index(attribute)]
+
+        #create root node
+        self.__root=Node(attribute,self.sortExamples(self.__data),options)
+
+        self.__root.print()
+
+        self.populateTree(self.__root)
+
+
+    def populateTree(self, parentNode):
+        options=parentNode.getOptions()
+        sorted_examples=parentNode.getSortedExamples()
+        attribute_pos=self.__attributes.index(parentNode.getAttribute())
+        sorted_examples_per_option=[]
+
+        for op in range(0,len(options)):
+            temp_examples=[]
+            for outcome in sorted_examples:
+                for example in outcome:
+                    if example[attribute_pos]==op:
+                        temp_examples.append(example)
+
+            temp_examples=self.sortExamples(temp_examples)
+            sorted_examples_per_option.append(temp_examples)
+        #check that it works by printing. it produces the examples sorted as in figure 18.4
+        #print(sorted_examples_per_option)
+
+        #walk through each option to see if a child shall be a leaf or node
+        for op in range(0,len(options)):
+            abc=(sorted_examples_per_option[op])
+            if len(sorted_examples_per_option[op][0])==0 and len(sorted_examples_per_option[op][1])==0:
+                focusNode=Node("Yes/no",[],[])
+                parentNode.addChild(focusNode)
+            elif len(sorted_examples_per_option[op][0])==0:
+                focusNode=Node("No",sorted_examples_per_option[op],[])
+                parentNode.addChild(focusNode)
+            elif len(sorted_examples_per_option[op][1])==0:
+                focusNode=Node("Yes",sorted_examples_per_option[op],[])
+                parentNode.addChild(focusNode)
+            else:
+                attributes=copy.copy(self.__attributes)
+                attributes.remove(parentNode.getAttribute())
+                attributes.remove(self.__goal)
+                child_attribute=self.importance(attributes,sorted_examples_per_option[op])
+                child_attribute_pos=self.__attributes.index(child_attribute)
+                child_options=self.__options[child_attribute_pos]
+                focusNode=Node(child_attribute,sorted_examples_per_option[op],child_options)
+                parentNode.addChild(focusNode)
+
+                #recursion
+                self.populateTree(focusNode)
+
 
 class Node:
     __attribute=""
-    __examples=[]
+    __sorted_examples=[]
     __options=[]
     __children=[]
 
     def __init__(self, attribute, examples, options):
         self.__attribute=attribute
-        self.__examples=examples
+        self.__sorted_examples=examples
         self.__options=options
 
     def getAttribute(self):
-        return self.getAttribute()
+        return self.__attribute
 
-    def getExamples(self):
-        return self.__examples
+    def getSortedExamples(self):
+        return self.__sorted_examples
 
     def getOptions(self):
         return self.__options
@@ -270,7 +336,7 @@ class Node:
 
     def print(self):
         print("Attribute: ", self.__attribute)
-        print("Examples: ", self.__examples)
+        print("Examples (sorted): ", self.__sorted_examples)
         print("Options: ", self.__options, ", with corresponding children: ", self.__children)
 
 
@@ -288,3 +354,5 @@ print(t.goalEntropy())
 
 t.buildTree()
 
+abc=t.getRoot()
+abc.print()
